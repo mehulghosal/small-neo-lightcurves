12c14e2825be1ed9eeb1d2d2489e0b3582ff65c8
Mehul Ghosal <mehul@hpc-dtn1.its.hawaii.edu>
2022-06-08 18:21:29 +0000

    why does it never findisfinish wtf

--- driver.py
+++ driver.py
@@ -15,7 +15,7 @@ print(len(f))
 # print(f)
 # index = sys.argv[1]
 #for index in range(len(f)-1, -1, -1):
-for index in range(20,len(f)):
+for index in range(21, len(f)):
 
 	print(f[index])
 	subprocess.run(['python3', 'magic_star.py', f[index, 0].split(' ')[1], str(L[index]), str(a[index])])#, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
