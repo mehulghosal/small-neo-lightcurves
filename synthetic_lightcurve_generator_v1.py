0bd8f18cad0f7d84d55cb0cfb4bb2f85f58b3353
brycebolin <pie314271@gmail.com>
2022-09-13 15:08:40 -0700

    Update synthetic_lightcurve_generator_v1.py
    changed amplitude input to mag, but output is still in flux

--- synthetic_lightcurve_generator_v1.py
+++ synthetic_lightcurve_generator_v1.py
@@ -20,33 +20,40 @@ argparse
 numpy
 
 directions for use and sample execution:
-python synthetic_lightcurve_generator_v1.py @if /Users/bolin/NEO/Follow_up/APO_observing/mehul_test/GE_1_lightcurves/1917070o13_lightcurve.txt @per 17.2 @amp 1000
+python synthetic_lightcurve_generator_v1.py @if /Users/bolin/NEO/Follow_up/APO_observing/mehul_test/GE_1_lightcurves/1917070o13_lightcurve.txt @per 17.2 @amp 0.2
+
+change log:
+
+2022-Sep-13: changed amplitude unites from flux to mag. The out put is still in time, flux, flux unc
 
 '''
 parser = argparse.ArgumentParser(prefix_chars='@')
 parser.add_argument("@if", "@@in_file", help="read in mehul's data in format of mjd, flux, flux unc")
 parser.add_argument("@per", "@@period", help="period in seconds")
-parser.add_argument("@amp", "@@amplitude", help="amplitude of lightcurve in flux units")
+parser.add_argument("@amp", "@@amplitude", help="amplitude of lightcurve in mag units")
 args = parser.parse_args()
 
 
 in_file = str(args.in_file)
 period_s = float(args.period)
-amplitude_flux = float(args.amplitude)
+amplitude_mag = float(args.amplitude)
 
 
-DCTAPO_date_MJD, DCTAPO_mag, DCTAPO_mag_unc = np.loadtxt(in_file).T
+DCTAPO_date_MJD, DCTAPO_flux, DCTAPO_flux_unc = np.loadtxt(in_file).T
 
 DCTAPO_date_seconds_from_start_s = (DCTAPO_date_MJD - DCTAPO_date_MJD[0]) * 3600. * 24.
-DCTAPO_mag_norm = DCTAPO_mag-np.median(DCTAPO_mag)
+DCTAPO_flux_norm = DCTAPO_flux-np.median(DCTAPO_flux)
 
 
-period_s = 17.174283947263444
+period_s = period_s
 f = 1/period_s
 
-time_difference_round_up_int_s = int(np.ceil(DCTAPO_date_seconds_from_start_s[-1]))
-A = np.mean([np.abs(np.max(DCTAPO_mag_norm)),np.abs(np.min(DCTAPO_mag_norm))])
-sig = np.median(DCTAPO_mag_unc)
+median_flux = np.median(DCTAPO_flux)
+
+flux_ratio = 10**(amplitude_mag/-2.5) #for lower peak
+
+A = ((1./flux_ratio) * median_flux) - median_flux
+sig = np.median(DCTAPO_flux_unc)
 
 time = DCTAPO_date_seconds_from_start_s
 mag_err_array = np.ones(len(time)) * sig
